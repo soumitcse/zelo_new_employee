7ddfc08b495241a4a7eb2b79dfd20661456b502d
soumitra <soumitcse@yahoo.com>
2016-02-24 13:47:20 +0530

    edit for first user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 # Enable it to Create first user
-#skip_before_action :authorize
+skip_before_action :authorize
   # GET /users
   # GET /users.json
   def index
